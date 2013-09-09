265ca56927ea59277e7500de331a6c0e24cd7583
cyonite <andreas.nurbo@gmail.com>
2013-09-09 22:25:14 +0200

    add renderingengine interface

---
+++ lib/interfaces/IRenderingEngine.php
@@ -0,0 +1,24 @@
+<?php
+namespace CLMVC\Interfaces;
+
+/**
+ * Class IRenderingEngine
+ * Defines an interface for rendering engines.
+ * This are used in controllers to parse the layout and
+ * views connected with a controller and action.
+ * @package CLMVC\Interfaces
+ */
+interface IRenderingEngine {
+    /**
+     * @return string mixed
+     */
+    public function getFileTypeSupport();
+
+    /**
+     * Returns the rendered content
+     * @param string $content
+     * @param array $tagged_content
+     * @return string
+     */
+    public function render($content, $tagged_content = array());
+}
