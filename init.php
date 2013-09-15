815333b961feea1c7d2683759e2beb64aaaac57f
cyonite <andreas.nurbo@gmail.com>
2013-09-15 14:27:48 +0200

    fix route regex

--- init.php
+++ init.php
@@ -4,17 +4,17 @@
  * @global MySqlDatabase $db
  */
 
-include(PACKAGEPATH . 'clmvc-autoloader.php');/*
-if (!defined('CLOUDLESS_CONFIG'))
-    if (file_exists(PACKAGEPATH.'config.php'))
-        include(PACKAGEPATH.'config.php');
-else
-    include CLOUDLESS_CONFIG;*/
-require PACKAGEPATH . 'lib/Helpers/ArraysHelper.php';
-if (file_exists(PACKAGEPATH . 'lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php'))
-    require PACKAGEPATH . 'lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php';
+include(PACKAGEPATH . '/clmvc-autoloader.php');
+if (!defined('CLOUDLESS_CONFIG')) {
+    if (file_exists(PACKAGEPATH.'/config.php'))
+        include(PACKAGEPATH.'/config.php');
+} else
+    include CLOUDLESS_CONFIG;
+require PACKAGEPATH . '/lib/Helpers/ArraysHelper.php';
+if (file_exists(PACKAGEPATH . '/lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php'))
+    require PACKAGEPATH . '/lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php';
 else {
-    trigger_error(sprintf('Cannot find viewengine file %s', PACKAGEPATH . 'lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php'), E_USER_ERROR);
+    trigger_error(sprintf('Cannot find viewengine file %s', PACKAGEPATH . '/lib/ViewEngines/' . ucfirst(VIEWENGINE) . '/setup.php'), E_USER_ERROR);
 }
 /*
 global $db;
