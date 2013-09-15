c508f6aa645bb902a4721e788ea0872d9ed923b3
cyonite <andreas.nurbo@gmail.com>
2013-09-15 19:26:26 +0200

    include index template and set response to 200 on rendered content

--- lib/controllers/Render/RenderedContent.php
+++ lib/controllers/Render/RenderedContent.php
@@ -2,15 +2,21 @@
 namespace CLMVC\Controllers\Render;
 
 class RenderedContent {
-    static $renderedContent;
+    private static $renderedContent;
+    private static $rendered = false;
 
     static function set($content) {
         self::$renderedContent = $content;
+        self::$rendered = true;
     }
     static function get() {
         return self::$renderedContent;
     }
     static function flush() {
         echo self::get();
+    }
+
+    static function hasRendered() {
+        return self::$rendered;
     }
 }
