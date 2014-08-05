be6de5b41d0b9fa76baa6a345edb289e9e61bb67
andreas nurbo <andreas.nurbo@gmail.com>
2014-08-05 21:08:26 +0200

    add current user can check

--- lib/Core/Security/SecurityHelpers.php
+++ lib/Core/Security/SecurityHelpers.php
@@ -1,15 +1,30 @@
 <?php
 
 /**
- * @param $userId
+ * @param $user
  * @param $capability
  * @param $data
+ * @internal param $userId
  * @return bool|mixed
  */
-function clmvc_user_can($userId, $capability, $data) {
-    if(user_can($userId, $capability)) {
+function clmvc_user_can($user, $capability, $data) {
+    if(user_can($user, $capability)) {
         return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
-            [user_can($userId, $capability), $data]);
+            [user_can($user, $capability), $data]);
+    }
+    return false;
+}
+
+
+/**
+ * @param string $capability
+ * @param mixed $data
+ * @return bool|mixed
+ */
+function cl_current_user_can($capability, $data) {
+    if(user_can(wp_get_current_user(), $capability)) {
+        return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
+            [user_can(wp_get_current_user(), $capability), $data]);
     }
     return false;
 }
