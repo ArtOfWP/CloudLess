ac7650286529c209e0ef308f19d17eb0356060ce
Andreas Nurbo <andreas.nurbo@gmail.com>
2015-04-16 20:50:14 +0200

    fix blocking user can calls

--- lib/Core/Security/SecurityHelpers.php
+++ lib/Core/Security/SecurityHelpers.php
@@ -7,11 +7,8 @@
  * @return bool|mixed
  */
 function clmvc_user_can($user, $capability, $data) {
-    if(user_can($user, $capability)) {
-        return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
-            [user_can($user, $capability), $data]);
-    }
-    return false;
+    return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
+        [user_can($user, $capability), $data]);
 }
 
 
@@ -21,9 +18,6 @@ function clmvc_user_can($user, $capability, $data) {
  * @return bool|mixed
  */
 function cl_current_user_can($capability, $data) {
-    if(current_user_can($capability)) {
-        return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
-            [user_can(wp_get_current_user(), $capability), $data]);
-    }
-    return false;
+    return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
+        [user_can(wp_get_current_user(), $capability), $data]);
 }
