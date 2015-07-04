61fed9979327543745cd2e7b74ebdc09ee572fe5
Andreas Nurbo <andreas.nurbo@gmail.com>
2015-07-04 15:29:52 +0200

    trigger warning if script and style dependencies are missing

--- lib/ViewEngines/WordPress/WpScriptIncludes.php
+++ lib/ViewEngines/WordPress/WpScriptIncludes.php
@@ -8,6 +8,14 @@ class WpScriptIncludes extends WpFrontIncludes
 {
     public function enqueueInclude(FrontInclude $script)
     {
+        if(defined('WP_DEBUG') && WP_DEBUG) {
+        $dependencies = $script->getDependency();
+            $scripts = wp_scripts()->registered;
+            foreach($dependencies as $dependency){
+                if(!isset($scripts[$dependency]))
+                    trigger_error(sprintf('Dependency failed for %s, No script with handle %s is registered', $script->getHandle(), $dependency), E_USER_WARNING);
+            }
+        }
         wp_enqueue_script($script->getHandle(), $script->getSrc(), $script->getDependency(), $script->getVersion(), $script->loadInFooter());
     }
 
--- lib/ViewEngines/WordPress/WpStyleIncludes.php
+++ lib/ViewEngines/WordPress/WpStyleIncludes.php
@@ -13,6 +13,14 @@ class WpStyleIncludes extends WpFrontIncludes
 {
     public function enqueueInclude(FrontInclude $style)
     {
+        if(defined('WP_DEBUG') && WP_DEBUG) {
+            $dependencies = $style->getDependency();
+            $scripts = wp_scripts()->registered;
+            foreach($dependencies as $dependency){
+                if(!isset($scripts[$dependency]))
+                    trigger_error(sprintf('Dependency failed for %s, No style with handle %s is registered', $style->getHandle(), $dependency), E_USER_WARNING);
+            }
+        }
         wp_enqueue_style($style->getHandle(), $style->getSrc(), $style->getDependency(), $style->getVersion(), $style->loadInFooter());
     }
 
