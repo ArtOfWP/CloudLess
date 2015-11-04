1dac00f97c43cd9e75072d0ba3662c8275d922d5
Andreas Nurbo <andreas.nurbo@gmail.com>
2015-11-04 13:23:06 +0100

    add wrapper for new function, add deprecated tag

--- lib/Core/Security/SecurityHelpers.php
+++ lib/Core/Security/SecurityHelpers.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * Filter:clmvc-user-can-{$capability}, [current_user_can(), $data, $user]
  * @param $user
  * @param $capability
  * @param $data
@@ -10,17 +11,29 @@
 function clmvc_user_can($user, $capability, $data)
 {
     return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
-        [user_can($user, $capability), $data]);
+        [user_can($user, $capability), $data, $user]);
 }
 
 /**
+ * @deprecated
+ * @param $capability
+ * @param null $data
+ * @return bool|mixed
+ */
+function cl_current_user_can($capability, $data = null)
+{
+    return clmvc_current_user_can($capability, $data);
+}
+
+/**
+ * Filter:clmvc-user-can-{$capability}, [current_user_can(), $data, $current_user]
  * @param string $capability
  * @param mixed $data
  *
  * @return bool|mixed
  */
-function cl_current_user_can($capability, $data = null)
+function clmvc_current_user_can($capability, $data = null)
 {
     return CLMVC\Events\Filter::run("clmvc-user-can-{$capability}",
-        [current_user_can($capability), $data]);
+        [current_user_can($capability), $data, wp_get_current_user()]);
 }
