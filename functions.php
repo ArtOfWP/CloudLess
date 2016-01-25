382b2a189db3e75db577de7aed5084ff211312a2
Andreas Nurbo <andreas.nurbo@gmail.com>
2016-01-25 13:21:05 +0100

    add MobileDetect and wrapper functions

--- functions.php
+++ functions.php
@@ -4,3 +4,21 @@
  */
 require __DIR__.'/lib/Helpers/ArraysHelper.php';
 require __DIR__.'/lib/Core/Security/SecurityHelpers.php';
+
+/**
+ * Checks if page is being viewed by a mobile.
+ * @return bool
+ */
+function clmvc_is_mobile() {
+    $mobile_detect=\CLMVC\Core\Container::instance()->fetchOrMake(\CLMVC\Helpers\MobileDetect::class);
+    return $mobile_detect->isMobile() && !clmvc_is_tablet();
+}
+
+/**
+ * Checks if page is being viewed by a tablet.
+ * @return bool
+ */
+function clmvc_is_tablet() {
+    $mobile_detect=\CLMVC\Core\Container::instance()->fetchOrMake(\CLMVC\Helpers\MobileDetect::class);
+    return $mobile_detect->isTablet();
+}
